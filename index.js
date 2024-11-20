949b60d6a78451a459a9fbe8ceb7ecf77cc53fa5
son-yeri <20201693@g.dongseo.ac.kr>
2024-11-21 01:23:10 +0900

    변경사항 정리..

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var random_1 = require("./random");
+console.log(random_1.coreCompetencyData);
--- random.ts
+++ random.ts
@@ -1,15 +1,32 @@
 import { randFloat } from '@ngneat/falso';
 import { CoreCompetencies } from './types'; // CoreCompetency 인터페이스 임포트
 
+// 대한민국 성씨 배열
+const koreanLastNames = [
+  '가', '간', '갈', '감', '강', '고', '곽', '구', '권', '김', '나', '남', '노', '도', '류', '마', '민', '박', '방', '서', '성', '손',
+  '송', '신', '양', '여', '오', '우', '유', '이', '임', '장', '전', '정', '조', '최', '한', '허', '홍'
+];
+
+// 랜덤 성씨를 생성하는 함수
+const generateRandomKoreanLastName = (): string => {
+  const randomIndex = Math.floor(Math.random() * koreanLastNames.length);
+  return koreanLastNames[randomIndex];
+};
+
+
+
 // 랜덤 퍼센트 값을 생성하는 함수
 const generateRandomPercentage = (): string => {
   return `${randFloat({ min: 0, max: 120, precision: 0.01 })}%`;
 };
 
-// CoreCompetency 객체에 랜덤 퍼센트 데이터 할당
+// CoreCompetency 객체에 랜덤 성 또는 랜덤 퍼센트 데이터 할당
 export const coreCompetencyData: CoreCompetencies = {
-  serlead: generateRandomPercentage(),
-  sympcomm: generateRandomPercentage(),
+  username: generateRandomKoreanLastName(),
+  email: 
+  password: 
+  serlead: generateRandomPercentage(), 
+  sympcomm: generateRandomPercentage(),  
   originality: generateRandomPercentage(),
   pliability: generateRandomPercentage(),
   knowledgequest: generateRandomPercentage(),
@@ -17,3 +34,14 @@ export const coreCompetencyData: CoreCompetencies = {
   globalmind: generateRandomPercentage(),
   asianculture: generateRandomPercentage(),
 };
+
+
+
+// const koreanLastNames = ['김', '이', '박', '최'];
+// const koreanFirstNames = ['민준', '서윤', '지우'];
+
+// const generateRandomFullName = (): string => {
+//   const lastName = koreanLastNames[Math.floor(Math.random() * koreanLastNames.length)];
+//   const firstName = koreanFirstNames[Math.floor(Math.random() * koreanFirstNames.length)];
+//   return `${lastName}${firstName}`;
+// };
